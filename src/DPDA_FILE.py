f83c50a0f13f4f697ea037f45c45941837d11625
Kerwyn Jean <kjean2@fordham.edu>
2025-11-26 01:58:23 -0500

    added two representatives for empty strings and botton stack symbols

---
+++ src/DPDA_FILE.py
@@ -0,0 +1,5 @@
+# DPDA_FILE.py (deterministic pushdown automata file operations)
+# dpda.py — DPDA for L = { a^n b^n } with one-symbol lookahead
+
+EPS = 'ε' # this represents the empty string (EPSILON) - no input
+BOT = '⊥' # this represents the bottom stack symbol
