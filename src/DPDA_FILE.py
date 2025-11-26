3ad1b8ab0fcaffdd3489d3d6dfcb5492496cbc08
Kerwyn Jean <kjean2@fordham.edu>
2025-11-26 03:30:35 -0500

    added more symbols and things to look out for

--- src/DPDA_FILE.py
+++ src/DPDA_FILE.py
@@ -4,9 +4,17 @@
 # Special Symbols
 EPS = 'ε' # this represents the empty string (EPSILON) - no input
 BOT = '⊥' # this represents the bottom stack symbol
+END = '$' # the input endmarker symbol
+
 P = 'p' # the pre-initial state (before pushing any inputs)
 Q = 'q' # the driver state (dispatches lookahead)
 QA = 'qa' # the accepting state (if next input symbol is 'a')
 QB = 'qb' # the accepting state (if next input symbol is 'b')
 QD = 'q$' # when the next input symbol is the endmarker '$'
 QACC = "q_accept" # the final accepting state 
+
+STATES = {P, Q, QA, QB, QD, QACC} # complete states that the DPDA can be in 
+FINAL_STATES = {QACC} # the ONLY final accepting state
+
+SIGMA = {"a", "b", END} # input alphabet (including endmarker '$')
+GAMMA = {"S", "a", "b", BOT} # the symbols that are allowed within the stack
